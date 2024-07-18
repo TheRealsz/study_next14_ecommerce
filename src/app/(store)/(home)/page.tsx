57749dc06ff6eb47a90c6a6b0d2c26374020208e
Robson Diego Andrade <Robindiegoa@gmail.com>
2024-07-18 18:02:09 -0300

    :sparkles: feat: create home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,7 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+            <Link href="/" className="relative group col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-baseline">
+                <Image
+                    src="/moletom-never-stop-learning.png"
+                    width={920}
+                    height={920}
+                    quality={100}
+                    alt="Imagem do produto"
+                    className="group-hover:scale-105 transition-transform"
+                />
+                <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+                    <span className="text-sm truncate">Moletom Never Stop Learning</span>
+                    <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold hover:bg-violet-700 transition-colors">R$219,90</span>
+                </div>
+            </Link>
+            <Link href="/" className="relative group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-baseline">
+                <Image
+                    src="/camiseta-dowhile-2022.png"
+                    width={460}
+                    height={460}
+                    quality={100}
+                    alt="Imagem do produto"
+                    className="group-hover:scale-105 transition-transform"
+                />
+                <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+                    <span className="text-sm truncate">Camiseta DoWhile 2022</span>
+                    <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold hover:bg-violet-700 transition-colors">R$109,90</span>
+                </div>
+            </Link>
+            <Link href="/" className="relative group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-baseline">
+                <Image
+                    src="/moletom-ai-side.png"
+                    width={460}
+                    height={460}
+                    quality={100}
+                    alt="Imagem do produto"
+                    className="group-hover:scale-105 transition-transform"
+                />
+                <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+                    <span className="text-sm truncate">Moletom AI Side</span>
+                    <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold hover:bg-violet-700 transition-colors">R$229,90</span>
+                </div>
+            </Link>
         </div>
     );
 }
