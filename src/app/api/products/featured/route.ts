6b17d3364e735b4d334b7c7925d327f9b03e7ba5
Robson Diego Andrade <Robindiegoa@gmail.com>
2024-07-19 22:37:49 -0300

    :sparkles: feat: creating route handlers

---
+++ src/app/api/products/featured/route.ts
@@ -0,0 +1,6 @@
+import { products } from "../data.json";
+
+export async function GET() {
+    const featuredProducts = products.filter((product) => product.featured);
+    return Response.json(featuredProducts);
+}
