151b3a454381797963323d2a0b0f0e13fd0ebb1d
Robson Diego Andrade <Robindiegoa@gmail.com>
2024-07-31 12:17:58 -0300

    :sparkles: feat: creating cart context

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/header";
+import { CartProvider } from "@/contexts/cart-context";
 
 
 export default function StoreLayout({
@@ -7,9 +8,11 @@ export default function StoreLayout({
     children: React.ReactNode;
 }>) {
     return (
-    <div className="mx-auto grid min-h-dvh w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
-        <Header />
-        {children}
-    </div>
-);
+        <CartProvider>
+            <div className="mx-auto grid min-h-dvh w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
+                <Header />
+                {children}
+            </div>
+        </CartProvider>
+    );
 }
---
+++ src/contexts/cart-context.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { createContext, useContext, useState } from "react";
+
+// Definir o tipo de um item do carrinho
+interface CartItem {
+    productId: string
+    quantity: number
+}
+
+// Definir o tipo do contexto e o que ele deve conter 
+interface CartContextType {
+    items: CartItem[];
+    addToCart: (productId: string) => void;
+}
+
+const CartContext = createContext({} as CartContextType);
+
+export function CartProvider({ children }: { children: React.ReactNode }) {
+
+    const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+    function addToCart(productId: string) {
+        // Verificando se o produto já está no carrinho
+        setCartItems((oldState) => {
+            const productInCart = oldState.some((item) => item.productId === productId);
+
+            // Se ele já estiver no carrinho, aumentar a quantidade após percorrer o array
+            if (productInCart) {
+                return oldState.map((item) => item.productId === productId
+                    ? { ...item, quantity: item.quantity + 1 }
+                    : item
+                );
+            }
+
+            // Caso nao esteja, adicionar o produto ao carrinho
+            else {
+                return [...oldState, { productId, quantity: 1 }];
+            }
+        });
+    }
+
+    return (
+        <CartContext.Provider value={{ items: cartItems, addToCart: addToCart }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+// Criar um hook para facilitar o uso do contexto
+export const useCart = () => useContext(CartContext);
