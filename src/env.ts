a9ed1aeba685f71cc234c0c41a086d2723006381
Robson Diego Andrade <Robindiegoa@gmail.com>
2024-08-10 22:30:11 -0300

    :sparkles: feat: creating client and server env boundary

--- src/env.ts
+++ src/env.ts
@@ -1,20 +1,20 @@
-import { z } from 'zod';
-
-const envSchema = z.object({
-    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+ 
+// Criando um schema para validar as variáveis de ambiente de servidor e cliente
+export const env = createEnv({
+  server: {
     APP_URL: z.string().url(),
+  },
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  },
+  // Definindo variáveis de ambiente que podem ser acessadas em tempo de execução. O NextJS acaba por verificar as variaveis nao utilizadas e nao as inclui no bundle.
+  // Fazendo isso, garantimos que elas serão incluídas no bundle e poderão ser acessadas em tempo de execução.
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
+
+  },
+
 });
-
-// Verificando se as variáveis de ambiente estão corretas conforme o schema
-const parsedEnv = envSchema.safeParse(process.env);
-
-if (!parsedEnv.success) {
-    // Flatten deixo os erros mais legíveis
-    console.error('Invalid environment variables:', parsedEnv.error.flatten().fieldErrors);
-    // Lançando um erro para que o build falhe caso as variáveis de ambiente não estejam corretas
-    throw new Error('Invalid environment variables');
-}
-
-
-// Variaveis ambientes já validadas
-export const env = parsedEnv.data;
